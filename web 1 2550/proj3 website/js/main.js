976c42ee152f49440be1a04832fa7da82042361e
zhforge <thezachcarter1449@gmail.com>
2025-10-01 15:12:29 -0600

    removed duplication

--- web 1 2550/proj3 website/js/main.js
+++
@@ -1,8 +0,0 @@
-function openSubsection(subsectionName) {
-    var i;
-    var x = document.getElementsByClassName("subsection");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(subsectionName).style.display = "grid";
-}
